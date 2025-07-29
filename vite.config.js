54aa8ad85ea81ed0ffa12e7d0e44f0cf20b32154
Diogo939405 <Diogo.Favilla@gmail.com>
2025-07-29 11:04:57 -0300

    ok

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const base = import.meta.env.VITE_BASE_PATH || '/ortizSystemFront'
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: base,
 })
